ec97d565645d401539e361723f2e6f78153e9060
Pouya <pouyamohammadyirbu@gmail.com>
2025-10-01 13:33:21 +0330

    fix: routing

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/pm-cloudify/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
